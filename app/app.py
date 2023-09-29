aebd5f4b8eec48f4cf407d1c86ce4f27618832f4
ibm-coder <146322114+ibm-coder@users.noreply.github.com>
2023-09-29 13:42:15 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, everyone!,Good Afternoon'
+    return 'Rk, Hello, everyone!,Good Afternoon'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
